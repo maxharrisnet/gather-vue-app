a0fc98bce85e6da99a7482348013c1f74fa64b83
Max Harris <maxreuven@gmail.com>
2019-03-02 01:03:38 -0800

    fixing

--- src/main.js
+++ src/main.js
@@ -1,24 +1,10 @@
 import Vue from 'vue'
-import App from './App.vue'
 import './plugins/vuetify'
+import App from './App.vue'
 import Vuetify from 'vuetify'
-
-import 'vuetify/dist/vuetify.min.css' // Ensure you are using css-loader
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
-
-Vue.use(Vuetify, {
-  theme: {
-    "primary": "#6200ea",
-    "secondary": "#2196f3",
-    "accent": "#00b0ff",
-    "error": "#ef5350",
-    "info": "#29b6f6",
-    "success": "#00e676",
-    "warning": "#ffca28"
-  }
-})
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,5 +1,4 @@
 import Vue from 'vue'
-import Vuetify from 'vuetify'
+import Vuetify from 'vuetify/lib'
 import 'vuetify/src/stylus/app.styl'
-import colors from 'vuetify/es5/util/colors'
 import 'vuetify/dist/vuetify.min.css'
